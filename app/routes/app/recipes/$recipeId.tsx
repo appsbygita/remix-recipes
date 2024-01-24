cb77eb1e9249e517ab245dcdc3a0b81689b3e4ad
Gita Ventyana <gita.ventyana@gmail.com>
2024-01-24 23:45:10 +0700

    Add ability to update recipe

--- app/routes/app/recipes/$recipeId.tsx
+++ app/routes/app/recipes/$recipeId.tsx
@@ -1,15 +1,21 @@
-import { json, type LoaderFunctionArgs } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
+import {
+  type ActionFunctionArgs,
+  json,
+  type LoaderFunctionArgs,
+} from "@remix-run/node";
+import { Form, useActionData, useLoaderData } from "@remix-run/react";
 import { Fragment } from "react";
+import { z } from "zod";
 import {
   DeleteButton,
   ErrorMessage,
   Input,
   PrimaryButton,
 } from "~/components/form";
-import { TimeIcon, TrashIcon } from "~/components/icons";
+import { SaveIcon, TimeIcon, TrashIcon } from "~/components/icons";
 import db from "~/db.server";
 import { classNames } from "~/utils/misc";
+import { validateForm } from "~/utils/validation";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const recipe = await db.recipe.findUnique({
@@ -21,6 +27,9 @@ export async function loader({ params }: LoaderFunctionArgs) {
           amount: true,
           name: true,
         },
+        orderBy: {
+          createdAt: "asc",
+        },
       },
     },
   });
@@ -28,8 +37,83 @@ export async function loader({ params }: LoaderFunctionArgs) {
   return json({ recipe }, { headers: { "Cache-Control": "max-age=10" } });
 }
 
+const saveRecipeSchema = z
+  .object({
+    name: z.string().min(1, "Name cannot be blank"),
+    totalTime: z.string().min(1, "Total time cannot be blank"),
+    instructions: z.string().min(1, "Instructions cannot be blank"),
+    ingredientIds: z
+      .array(z.string().min(1, "Ingredient ID is missing"))
+      .optional(),
+    ingredientAmounts: z.array(z.string().nullable()).optional(),
+    ingredientNames: z
+      .array(z.string().min(1, "Name cannot be blank"))
+      .optional(),
+  })
+  .refine(
+    (data) =>
+      data.ingredientIds?.length === data.ingredientAmounts?.length &&
+      data.ingredientIds?.length === data.ingredientNames?.length,
+    { message: "Ingredient arrays must all be the same length" }
+  );
+
+const createIngredientSchema = z.object({
+  newIngredientAmount: z.string().nullable(),
+  newIngredientName: z.string().min(1, "Name cannot be blank"),
+});
+
+export async function action({ request, params }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const recipeId = String(params.recipeId);
+
+  switch (formData.get("_action")) {
+    case "saveRecipe": {
+      return validateForm(
+        formData,
+        saveRecipeSchema,
+        ({ ingredientIds, ingredientNames, ingredientAmounts, ...data }) =>
+          db.recipe.update({
+            where: { id: recipeId },
+            data: {
+              ...data,
+              ingredients: {
+                updateMany: ingredientIds?.map((id, index): any => ({
+                  where: { id },
+                  data: {
+                    amount: ingredientAmounts?.[index],
+                    name: ingredientNames?.[index],
+                  },
+                })),
+              },
+            },
+          }),
+        (errors) => json({ errors }, { status: 400 })
+      );
+    }
+    case "createIngredient": {
+      return validateForm(
+        formData,
+        createIngredientSchema,
+        ({ newIngredientAmount, newIngredientName }) =>
+          db.ingredient.create({
+            data: {
+              recipeId,
+              amount: String(newIngredientAmount),
+              name: newIngredientName,
+            },
+          }),
+        (errors) => json({ errors }, { status: 400 })
+      );
+    }
+    default: {
+      return null;
+    }
+  }
+}
+
 export default function RecipeDetail() {
   const data = useLoaderData<typeof loader>();
+  const actionData = useActionData<any>();
 
   return (
     <Form method="post" reloadDocument>
@@ -42,8 +126,9 @@ export default function RecipeDetail() {
           className="text-2xl font-extrabold"
           name="name"
           defaultValue={data.recipe?.name}
+          error={!!actionData?.errors?.name}
         />
-        <ErrorMessage></ErrorMessage>
+        <ErrorMessage>{actionData?.errors?.name}</ErrorMessage>
       </div>
       <div className="flex">
         <TimeIcon />
@@ -55,39 +140,70 @@ export default function RecipeDetail() {
             autoComplete="off"
             name="totalTime"
             defaultValue={data.recipe?.totalTime}
+            error={!!actionData?.errors?.totalTime}
           />
-          <ErrorMessage></ErrorMessage>
+          <ErrorMessage>{actionData?.errors?.totalTime}</ErrorMessage>
         </div>
       </div>
       <div className="grid grid-cols-[30%_auto_min-content] my-4 gap-2">
         <h2 className="font-bold text-sm pb-1">Amount</h2>
         <h2 className="font-bold text-sm pb-1">Name</h2>
         <div></div>
-        {data.recipe?.ingredients.map((ingredient) => (
+        {data.recipe?.ingredients.map((ingredient, idx) => (
           <Fragment key={ingredient.id}>
+            <input type="hidden" name="ingredientIds[]" value={ingredient.id} />
             <div>
               <Input
                 type="text"
                 autoComplete="off"
-                name="ingredientAmount"
+                name="ingredientAmounts[]"
                 defaultValue={ingredient.amount ?? ""}
+                error={!!actionData?.errors?.[`ingredientAmounts.${idx}`]}
               />
-              <ErrorMessage></ErrorMessage>
+              <ErrorMessage>
+                {actionData?.errors?.[`ingredientAmounts.${idx}`]}
+              </ErrorMessage>
             </div>
             <div>
               <Input
                 type="text"
                 autoComplete="off"
-                name="ingredientName"
+                name="ingredientNames[]"
                 defaultValue={ingredient.name}
+                error={!!actionData?.errors?.[`ingredientNames.${idx}`]}
               />
-              <ErrorMessage></ErrorMessage>
+              <ErrorMessage>
+                {actionData?.errors?.[`ingredientNames.${idx}`]}
+              </ErrorMessage>
             </div>
             <button>
               <TrashIcon />
             </button>
           </Fragment>
         ))}
+        <div>
+          <Input
+            type="text"
+            autoComplete="off"
+            name="newIngredientAmount"
+            className="border-b-gray-200"
+            error={!!actionData?.errors?.newIngredientAmount}
+          />
+          <ErrorMessage>{actionData?.errors?.newIngredientAmount}</ErrorMessage>
+        </div>
+        <div>
+          <Input
+            type="text"
+            autoComplete="off"
+            name="newIngredientName"
+            className="border-b-gray-200"
+            error={!!actionData?.errors?.newIngredientName}
+          />
+          <ErrorMessage>{actionData?.errors?.newIngredientName}</ErrorMessage>
+        </div>
+        <button name="_action" value="createIngredient">
+          <SaveIcon />
+        </button>
       </div>
       <label
         htmlFor="instructions"
@@ -103,14 +219,15 @@ export default function RecipeDetail() {
         defaultValue={data.recipe?.instructions}
         className={classNames(
           "w-full h-56 rounded-md outline-none",
-          "focus:border-2 focus:p-3 focus:border-primary duration-300"
+          "focus:border-2 focus:p-3 focus:border-primary duration-300",
+          actionData?.errors?.instructions ? "border-2 border-red-500 p-3" : ""
         )}
       />
-      <ErrorMessage></ErrorMessage>
+      <ErrorMessage>{actionData?.errors?.instructions}</ErrorMessage>
       <hr className="my-4" />
       <div className="flex justify-between">
         <DeleteButton>Delete this Recipe</DeleteButton>
-        <PrimaryButton>
+        <PrimaryButton name="_action" value="saveRecipe">
           <div className="flex flex-col justify-center h-full">Save</div>
         </PrimaryButton>
       </div>
